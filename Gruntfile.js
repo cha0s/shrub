76a7eedef9dc6f607c9c52f13243a0981f1ba01d
Ruben Rodriguez II <cha0s@therealcha0s.net>
2016-08-24 22:22:51 -0500

    Updated dependencies, and added JS Gruntfile shim

---
+++ Gruntfile.js
@@ -0,0 +1,5 @@
+// This is just an ugly shim since Grunt doesn't support CoffeeScript
+// Gruntfiles anymore apparently... Jerks.
+
+require('coffee-script/register');
+module.exports = require('./Gruntfile.litcoffee');
